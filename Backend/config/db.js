c6c83376651778caed79d4c46efcdd057e90a3d9
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-07-24 00:02:49 +0530

    ..

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,12 +3,7 @@ import { ENV_VARS } from "./envVars.js";
 
 export const connectDB=async ()=>{
     try {
-        const conn=await mongoose.connect(ENV_VARS.MONGODB_URI, {
-
-            useNewUrlParser: "true",
-            useUnifiedTopology: "true"
-
-          })
+        const conn=await mongoose.connect(ENV_VARS.MONGO_URL)
         console.log("MongoDB connected..."+ conn.connection.host);
     } catch (error) {
         console.error(`Error connecting to MongoDB: ${error.message}`);
