a488ebada35d360cf0da43eabcd5482f98d0a58e
Yasas_Isuru <96908186+Yasasisuru@users.noreply.github.com>
2024-07-24 20:51:41 +0530

    add

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -1,5 +1,55 @@
+import { User } from '../models/user.model.js';
+
 export async function signup(req,res){
-    res.send("signup route");
+   try {
+    const{email,password,username}= req.body;
+
+    if(!email || !password || !username){
+        return res.status(400).json({success:false,message:"All Fields Are Required"})
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!emailRegex.test(email)){
+        return res.status(400).json({success:false,message:"Invalid-Email"})
+    }
+
+    if(password.length <8){
+        return res.status(400).json({success:false,message:"Password Must be least that 8 characters"})
+
+    }
+
+    const existingByEmail= await User.findOne({email:email})
+
+    if(existingByEmail){
+        return res.status(400).json({success:false,message:"Email Already Exists"})
+    }
+
+
+    const existingByUsername= await User.findOne({username:username})
+
+    if(existingByUsername){
+        return res.status(400).json({success:false,message:"Username Already Exists"})
+    }
+
+    const PROFILE_PICS = ["/avatar1.png","/avatar2.jpg","/avatar3.png"];
+    const image=PROFILE_PICS[Math.floor(Math.random()*PROFILE_PICS.length)];
+
+    const newUser = new User({
+        email,
+        password,
+        username,
+        image
+    })
+
+    await newUser.save()
+
+
+
+
+   } catch (error) {
+    console.log("ERROR in sign-up Controller", error.message)
+    res.status(500).json({success:false,message:"Internal server ERROR"})
+   }
 }
 
 export async function login(req,res){
