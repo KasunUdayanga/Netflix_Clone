1312e021149f8a2646757caf780faf75a3642d5c
janitha Bulathwatta <janithabulathwatta04@gmail.com>
2024-07-27 17:24:41 +0530

    add TMDB api in the backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,25 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.route.js';
-import { ENV_VARS } from './config/envVars.js';
-import { connectDB } from './config/db.js';
+import express from "express";
+
+import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js"; 
+
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
 
 
-dotenv.config();
+ //this line
 const app = express();
+
 const PORT=ENV_VARS.PORT;
+
 app.use(express.json());
-app.use("/api/v1/auth",authRoutes)
+
+app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/movie",movieRoutes);
+
 
 app.listen(PORT,()=>{
     console.log('Server is running in http://localhost:'+PORT);
    connectDB();
-})
+});
+
