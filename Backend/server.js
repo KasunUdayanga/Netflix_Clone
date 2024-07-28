e7978920d2c2ac3d641cb8e929ead2fc84a90763
HimanthaHK <plghkhimantha@gmail.com>
2024-07-28 13:42:05 +0530

    add api

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js"; 
+import tvRoutes from "./routes/tv.route.js"; 
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use("/api/v1/auth",authRoutes);
 app.use("/api/v1/movie",movieRoutes);
+app.use("/api/v1/movie",tvRoutes);
 
 
 app.listen(PORT,()=>{
