30d29d31344cc87028dcc2fb6ca15c3cfb680e4a
Yasas_Isuru <96908186+Yasasisuru@users.noreply.github.com>
2024-08-04 12:38:31 +0530

    update interface in  AuthScreen

--- Frontend/src/pages/home/AuthScreen.jsx
+++ Frontend/src/pages/home/AuthScreen.jsx
@@ -49,8 +49,109 @@ function AuthScreen() {
 
       {/*section -01 */}
       <div className="py-10 bg-black text-white">
-        
+        <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
+          {/* left side */}
+          <div className="flex-1 text-center md:text-left">
+            <h2 className="text-4xl md:text-5xl font-extrabold mb-4">
+              Enjoy on your TV
+            </h2>
+            <p className="text-lg md:text-xl">
+              Watch on Smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
+              Blu-ray players, and more..
+            </p>
+          </div>
+
+          {/* Right side */}
+
+          <div className="flex-1 relative">
+            <img src="/tv.png" alt="Tv image" className="mt-4 z-20 relative" />
+            <video
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-1/2 z-10"
+              playsInline
+              autoPlay={true}
+              muted
+              loop
+            >
+              <source src="/hero-vid.m4v" type="video/mp4" />
+            </video>
+          </div>
+        </div>
       </div>
+
+      <div className="h-2 w-full bg-[#232323]" aria-hidden="true" />
+
+      {/*section -02 */}
+      <div className='py-10 bg-black text-white'>
+				<div className='flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col-reverse px-4 md:px-2'>
+					{/* left side */}
+					<div className='flex-1 relative'>
+						<div className='relative'>
+							<img src='/stranger-things-lg.png' alt='Stranger Things img' className='mt-4' />
+
+							<div
+								className='flex items-center gap-2 absolute bottom-5 left-1/2 -translate-x-1/2 bg-black
+              w-3/4 lg:w-1/2 h-24 border border-slate-500 rounded-md px-2
+              '
+							>
+								<img src='/stranger-things-sm.png' alt='image' className='h-full' />
+								<div className=' flex justify-between items-center w-full'>
+									<div className='flex flex-col gap-0'>
+										<span className='text-md lg:text-lg font-bold'>Stranger Things</span>
+										<span className='text-sm text-blue-500'>Downloading...</span>
+									</div>
+
+									<img src='/download-icon.gif' alt='' className='h-12' />
+								</div>
+							</div>
+						</div>
+					</div>
+					{/* right side */}
+
+					<div className='flex-1 md:text-left text-center'>
+						<h2 className='text-4xl md:text-5xl font-extrabold mb-4 text-balance'>
+							Download your shows to watch offline
+						</h2>
+						<p className='text-lg md:text-xl'>
+							Save your favorites easily and always have something to watch.
+						</p>
+					</div>
+				</div>
+			</div>
+
+		
+
+			<div className='h-2 w-full bg-[#232323]' aria-hidden='true' />
+
+			{/* section -03 */}
+			<div className='py-10 bg-black text-white'>
+				<div className='flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2'>
+					{/* left side */}
+					<div className='flex-1 text-center md:text-left'>
+						<h2 className='text-4xl md:text-5xl font-extrabold mb-4'>Watch everywhere</h2>
+						<p className='text-lg md:text-xl'>
+							Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.
+						</p>
+					</div>
+
+					{/* right side */}
+					<div className='flex-1 relative overflow-hidden'>
+						<img src='/device-pile.png' alt='Device image' className='mt-4 z-20 relative' />
+						<video
+							className='absolute top-2 left-1/2 -translate-x-1/2  h-4/6 z-10
+               max-w-[63%] 
+              '
+							playsInline
+							autoPlay={true}
+							muted
+							loop
+						>
+							<source src='/video-devices.m4v' type='video/mp4' />
+						</video>
+					</div>
+				</div>
+			</div>
+
+			<div className='h-2 w-full bg-[#232323]' aria-hidden='true' />
     </div>
   );
 }
