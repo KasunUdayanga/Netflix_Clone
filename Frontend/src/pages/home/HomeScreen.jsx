e0786e192d42987cf5f64f5aa1ebe048fb81a98f
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-08-18 00:44:01 +0530

    add navbar

--- Frontend/src/pages/home/HomeScreen.jsx
+++ Frontend/src/pages/home/HomeScreen.jsx
@@ -1,11 +1,12 @@
+import Navbar from "../../components/Navbar";
 import { useAuthStore } from "../../store/authUser";
 
 const HomeScreen = () => {
     const { logout } = useAuthStore();
     return (
-        <div>
-            Home Screen
-            <button onClick={logout}>Logout</button>
+        <div className='h-screen text-white relative'>
+            <Navbar />
+            <div className='absolute top-0 left-0 w-full h-full bg-black/100 flex items-center justify-center -z-10 shimmer' />
         </div>
     );
 };
