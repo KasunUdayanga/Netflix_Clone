cb4410bc822d2df1559908b386406b91e0e651aa
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-08-20 07:56:50 +0530

    error handle

--- Backend/server.js
+++ Backend/server.js
@@ -23,12 +23,16 @@ app.use("/api/v1/auth",authRoutes);
 app.use("/api/v1/movie",protectRoute,movieRoutes);
 app.use("/api/v1/tv",protectRoute,tvRoutes);
 app.use("/api/v1/search",protectRoute,searchRoutes);
-if (ENV_VARS.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname,'frontend', 'dist', 'index.html'));
-    });
+
+
+if (ENV_VARS.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
+
 
 app.listen(PORT,()=>{
     console.log('Server is running in http://localhost:'+PORT);
